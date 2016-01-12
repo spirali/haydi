55551d48bebcbd4f8e8b65dc2d5584687aca62b7
Jakub Beránek <berykubik@gmail.com>
2016-01-12 22:18:00 +0100

    ENH: changed draph generation

--- src/qit/graph.py
+++ src/qit/graph.py
@@ -1,43 +1,32 @@
-class GraphNode(object):
-    def __init__(self, child, data, height):
-        self.child = child
-        self.data = data
-        self.height = height
+class Node(object):
+    def __init__(self, inputs, output, iterator):
+        self.inputs = inputs
+        self.output = output
+        self.iterator = iterator
 
 
-class IteratorGraph(object):
+class Graph(object):
+    @staticmethod
+    def _construct(iterator):
+        nodes = []
+        queue = [(iterator, None)]  # (iterator, parent node)
+
+        while len(queue) > 0:
+            item = queue.pop()
+
+            new_node = Node([], None, item[0])
+
+            if item[1]:  # if this is a parent of a node, register itself as input and set the parent as output
+                item[1].inputs.append(new_node)
+                new_node.output = item[1]
+
+            for parent in item[0].get_parents():
+                queue.append((parent, new_node))
+
+            nodes.append(new_node)
+
+        return nodes
+
     def __init__(self, iterator):
-        self.origin_iterator = iterator
-        self.graph = self._construct(iterator)
-
-    def __iter__(self):
-        return IteratorGraph(self.origin_iterator)
-
-    def next(self):
-        if self.graph is None:
-            raise StopIteration()
-        else:
-            node = self.graph
-            self.graph = node.child
-            return node
-
-    def _construct(self, iterator):
-        height = 0
-        current_level = [iterator]
-        next_level = []
-        root = None
-
-        while True:
-            for it in current_level:
-                next_level += it.get_parents()
-
-            root = GraphNode(root, current_level, height)
-            height += 1
-
-            if len(next_level) < 1:
-                break
-            else:
-                current_level = next_level
-                next_level = []
-
-        return root
+        self.nodes = Graph._construct(iterator)
+        self.origin_node = self.nodes[0]
