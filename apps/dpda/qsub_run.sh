869c1403a46d5ef7f4a23d6ac449ec9e2872e50e
Jakub Beránek <berykubik@gmail.com>
2016-12-17 15:22:30 +0100

    ENH: count workers after cluster initialization

--- apps/dpda/qsub_run.sh
+++ apps/dpda/qsub_run.sh
@@ -5,14 +5,29 @@
 #XPBS -A IT4I-8-2
 #PBS -N haydi-test
 
+SERVER="127.0.0.1"
+PORT=9020
+PROJECT_PATH="~/projects/pyqit"
+
 source /etc/profile
 
 module load Python/2.7.9-GNU-5.1.0-2.25
 export PATH=~/.local/bin:$PATH
 source ~/.local/bin/virtualenvwrapper.sh
 
-~/projects/pyqit/it4i/start-dist.sh
-cd $PBS_O_WORKDIR
-sleep 6
+${PROJECT_PATH}/it4i/start-dist.sh
+
+# wait for cluster to start
+WORKER_COUNT=$(wc -l < $PBS_NODEFILE)
+WORKER_COUNT=$(($WORKER_COUNT * 24))
+echo "Waiting for $WORKER_COUNT workers..."
+
+while [ `python ${PROJECT_PATH}/it4i/count_workers.py "$SERVER:$PORT"` -ne ${WORKER_COUNT} ]; do
+        sleep 1;
+done
+echo "Cluster started"
+
+cd ${PBS_O_WORKDIR}
+
 workon pypy
-time python eqlevel.py --scheduler=127.0.0.1 --port=9010
+time python eqlevel.py --scheduler=${SERVER} --port=${PORT}
