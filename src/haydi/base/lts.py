f13d813cc23bea3f77ccd31669624d492cefcf65
Stanislav Bohm <stanislav.bohm@vsb.cz>
2017-02-17 13:12:23 +0100

    FIX: Fixed iterator for bfs_path

--- src/haydi/base/lts.py
+++ src/haydi/base/lts.py
@@ -23,7 +23,7 @@ class DLTS(object):
                                         max_depth,
                                         return_depth,
                                         max_states)
-        domain = Domain(None, False)
+        domain = Domain(None, False, name="bfs")
         domain.create_iter = create_iter
         return domain
 
@@ -31,13 +31,14 @@ class DLTS(object):
                  init_state,
                  max_depth=sys.maxint,
                  max_states=sys.maxint):
-        def create_iterator():
+        def create_iter(step):
+            assert step == 0
             return BreadthFirstIterator2(self,
                                          init_state,
                                          max_depth,
                                          max_states)
-        domain = Domain(None, False)
-        domain.create_iterator = create_iterator
+        domain = Domain(None, False, name="bfs_path")
+        domain.create_iter = create_iter
         return domain
 
     def get_enabled_actions(self, state):
